63f78011ac9d655aeab4671ce6490283b5de84f1
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-12-08 09:28:26 +0000

    Added checkout form data caching in payment intent

--- dreamsfitness/settings.py
+++ dreamsfitness/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = 'django-insecure-vjf7%iz9e_j0o_a4m#j64ku*s5t^tyj21cpuak3m$#dzp5kq(e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',  # vs code preview
+    'localhost',  # listen to stripe webhooks
+    ]
 
 
 # Application definition
@@ -181,6 +184,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
