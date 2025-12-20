68ec0d3ba48aadfec3836510eb8d4234e7b27cbb
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-12-20 18:05:34 +0000

    Dashboard - Admin with product and subscription management

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 
 from products.models import Product
-from subscriptions.models import SubPlan, Subscription
+from subscriptions.models import SubPlan, SubPlanFeature, PlanDiscount
 
 
 @staff_member_required
 def admin_dashboard(request):
-    """ Admin dashboard: products + subscriptions """
-
-    products = Product.objects.all()
     plans = SubPlan.objects.all()
-    subscriptions = Subscription.objects.all()
+    features = SubPlanFeature.objects.all()
+    discounts = PlanDiscount.objects.all()
+    products = Product.objects.all()
 
     context = {
-        "products": products,
         "plans": plans,
-        "subscriptions": subscriptions,
+        "features": features,
+        "discounts": discounts,
+        "products": products,
     }
 
-    return render(request, "dashboard/admin_dashboard.html", context)
+    return render(
+        request, "dashboard/admin_dashboard.html", context
+    )
