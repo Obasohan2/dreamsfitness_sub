700cb0af76cd4ea772ec5d35b4deee83f5adbdbb
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-12-31 12:09:39 +0000

    Fix 500 errors: remove broken include and fix widget template path

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,52 @@
 from django.contrib import admin
+
 from .models import BlogPost, Comment
 
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'created_on')
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = (
+        "title",
+        "author",
+        "created_on",
+    )
+    list_filter = (
+        "created_on",
+        "author",
+    )
+    search_fields = (
+        "title",
+        "body",
+    )
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+    ordering = (
+        "-created_on",
+    )
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'post', 'created_on')
-    readonly_fields = ('user', 'post', 'created_on')
+    list_display = (
+        "user",
+        "post",
+        "created_on",
+    )
+    list_filter = (
+        "created_on",
+        "user",
+    )
+    search_fields = (
+        "body",
+        "user__username",
+    )
+    readonly_fields = (
+        "user",
+        "post",
+        "created_on",
+    )
+    ordering = (
+        "created_on",
+    )
+
