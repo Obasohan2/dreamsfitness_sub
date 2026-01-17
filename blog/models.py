8b6d412b4053167069e2fc0a737aa0ece10d765c
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2026-01-17 14:30:39 +0000

    Fix BlogPost model to match live database schema

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Category(models.Model):
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=130, unique=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
